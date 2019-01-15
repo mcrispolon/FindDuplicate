8e4d5a9ff5018c18933fafd9456c8daa9510a005
mcrispolon <crispolon12.maryann@gmail.com>
2019-01-15 18:23:36 -0500

    Initial Commit

---
+++ DuplicateFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindDuplicateFile
+{
+   public class DuplicateFile
+    {
+        public string FileHash { get; set; }
+        public string FileName { get; set; }
+        public string FileDirectory { get; set; }
+    }
+
+
+    public class FileGrouping
+    {
+        public string File { get; set; }
+
+        public List<DuplicateFile> Files { get; set; }
+    }
+
+
+}
